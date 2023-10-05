6a39bfe140399c32a8d72200fb1e956cba57e8d2
Abe Flansburg <abeflansburg@gmail.com>
2023-10-05 16:04:43 -0500

    add extra index urls

--- composer_local_dev/docker_files/entrypoint.sh
+++ composer_local_dev/docker_files/entrypoint.sh
@@ -25,7 +25,22 @@ mkdir -p ${AIRFLOW__CORE__PLUGINS_FOLDER}
 # `python` to python3 exec, in Composer >= 1.19.2 name `python` is already
 # linked to python3 and file no longer exist.
 if [ -f /var/local/setup_python_command.sh ]; then
-    /var/local/setup_python_command.sh
+  /var/local/setup_python_command.sh
+fi
+
+if [ -n "${PRIVATE_INDEX_URLS}" ]; then
+  echo "Adding private PyPI repository indexes: ${PRIVATE_INDEX_URLS}"
+  pip3 install keyring keyrings.google-artifactregistry-auth
+
+  # Split PRIVATE_INDEX_URLS by comma and create a space-separated string of URLs
+  IFS=',' read -ra URLS_ARRAY <<<"${PRIVATE_INDEX_URLS}"
+  EXTRA_INDEX_URLS=""
+  for url in "${URLS_ARRAY[@]}"; do
+    EXTRA_INDEX_URLS+=" --extra-index-url ${url}"
+  done
+
+  # Use the URLs in pip config set
+  pip3 config set global.extra-index-url "${EXTRA_INDEX_URLS}"
 fi
 
 pip3 install --upgrade -r composer_requirements.txt
@@ -35,7 +50,7 @@ airflow db init
 
 # Allow non-authenticated access to UI for Airflow 2.*
 if ! grep -Fxq "AUTH_ROLE_PUBLIC = 'Admin'" /home/airflow/airflow/webserver_config.py; then
-  echo "AUTH_ROLE_PUBLIC = 'Admin'" >> /home/airflow/airflow/webserver_config.py
+  echo "AUTH_ROLE_PUBLIC = 'Admin'" >>/home/airflow/airflow/webserver_config.py
 fi
 
 airflow scheduler &
