3725e15c19eda481afafa09c4a95601da3c1ae0b
Thomas Randazzo <thomas.randazzo@brightside.com>
2024-07-01 09:17:19 -0500

    get requirements to run extra_index_url

--- composer_local_dev/docker_files/entrypoint.sh
+++ composer_local_dev/docker_files/entrypoint.sh
@@ -49,7 +49,7 @@ if [ -n "${PRIVATE_INDEX_URLS}" ]; then
 fi
 
 # Example usage of PIP_EXTRA_INDEX_URL in pip install
-pip3 install --upgrade -r composer_requirements.txt 'extra_index_url='"${PIP_EXTRA_INDEX_URL}"
+pip3 install --upgrade -r composer_requirements.txt --extra_index_url="${PIP_EXTRA_INDEX_URL}"
 
 pip3 install --upgrade -r composer_requirements.txt 'extra_index_url='"${PIP_EXTRA_INDEX_URL}"
 pip3 check
